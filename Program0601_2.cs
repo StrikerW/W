ffc46000fc72949a2b755307b9a3d64530145465
WANLING YU <yu970809@outlook.com>
2020-06-01 02:16:39 -0700

    task

---
+++ Program0601_2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the first number:");// get the number from the keyboard
+            string num1 = Console.ReadLine();//read the number
+            int number1 = Convert.ToInt32(num1);//convert string into int
+            Console.Write("input the second number:");
+            string num2 = Console.ReadLine();
+            int number2 = Convert.ToInt32(num2);
+            Console.Write(Environment.NewLine);
+            int sum = number1 + number2;
+            Console.WriteLine("sum is {0}",sum);
+        }
+    }
+}
